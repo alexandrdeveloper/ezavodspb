4bcc5f7cd48ec66ec96bb9207607f0c93664e3c0
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-05-30 20:48:48 +0300

    object section

--- js/common.js
+++ js/common.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
 	let headerSlider = $('.header-slider');
+	let eventsSlider = $('.events__content');
+	let objectImgSlider = $('.object__image');
+	let objectInfoSlider = $('.object__info');
 
 	headerSlider.slick({
 		slidesToShow: 1,
@@ -12,11 +15,44 @@ $(document).ready(function() {
 		touchMove: false
 	});
 
-	$('.events__content').slick({
+	eventsSlider.slick({
 		slidesToShow: 2,
 		centerMode: true,
 		centerPadding: '200px'
 	});
+	let currentOutput = $('.slides-num__current');
+	let generalOutput = $('.slides-num__general');
+
+	objectImgSlider.on('init reInit afterChange', function (event, slick, currentSlide) {
+		let i = (currentSlide ? currentSlide : 0) + 1;    	
+    	
+    	currentOutput.text(`00${i}`);    	
+    	
+    	generalOutput.text(`00${slick.slideCount}`);
+	});
+
+	objectImgSlider.slick({
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		speed: 1000,
+		cssEase: 'ease-in-out',
+		asNavFor: $('.object__info')
+	});
+
+	
+
+	objectInfoSlider.slick({
+		slidesToShow: 1,
+		fade: true,
+		asNavFor: $('.object__image'),
+		speed: 1000,
+	});
+
+
+
+
 
 
 });
